eaa820db4a751226dc717fd5be1a03bf3cac9af5
Astrid <tingan.l1216@gmail.com>
2021-08-07 16:38:02 +0800

    make it to be MVC

---
+++ week2/Simple App/Simple App/Controller/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Simple App
+//
+//  Created by Astrid on 2021/8/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    @IBOutlet weak var middleLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    var randomNumber = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        update()
+  }
+   
+    @IBAction func pressFunFact(_ sender: UIButton) {
+       update()
+    }
+    
+   func update(){
+      
+    let newNumber = Int.random(in: 0 ..< Content.text.count)
+        
+        if newNumber != randomNumber {
+            randomNumber = newNumber
+        } else if newNumber == randomNumber && randomNumber < Content.text.count - 1
+        {
+            randomNumber = newNumber + 1
+        } else {
+            randomNumber = newNumber - 1
+        }
+        
+       print("randomNumber:\(randomNumber)")
+        
+        // the text color on button have to be managed
+        
+    view.backgroundColor = UIColor(hexString: Content.color[randomNumber])
+    funFactButton.titleLabel?.textColor = UIColor(hexString: Content.color[randomNumber])
+    middleLabel.text = Content.text[randomNumber]
+  }
+
+}
+
---
+++ week2/Simple App/Simple App/Modal/Content.swift
@@ -0,0 +1,31 @@
+//
+//  Content.swift
+//  Simple App
+//
+//  Created by Astrid on 2021/8/7.
+//
+
+import Foundation
+
+enum Content {
+    
+    static let text = [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas tempus.",
+        "Contrary to popular belief, Lorem Ipsum is not simply random text.",
+        "Richard McClintock, a Latin professor at Hampden-Sydney College in ",
+        "looked up one of the more obscure Latin words, consectetur",
+        "from a Lorem Ipsum passage, and going through the cities of the word",
+        "This book is a treatise on the theory of ethics, very popular during the.",
+        "The first line of Lorem Ipsum, Lorem ipsum dolor sit amet..",
+    ]
+    
+    static let color = [
+        "be6e46",
+        "cde7b0", //太刺眼要換掉
+        "A3BFA8",
+        "7286A0",
+        "59594A",
+        "4F5D75",
+        "A59132",
+    ]
+}
