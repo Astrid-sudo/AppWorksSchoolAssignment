7bd6eb56511f8979738cde1db498f6590aabd934
Astrid <tingan.l1216@gmail.com>
2021-08-10 23:51:48 +0800

    adjust UI,IBOutletAction done,segmentControl almost done

--- week3/logInSignUpPage/logInSignUpPage/ViewController.swift
+++ week3/logInSignUpPage/logInSignUpPage/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var checkTextField: UITextField!
+    @IBOutlet weak var checkLabel: UILabel!
+    @IBAction func pressButton(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        segmentControl.layer.borderWidth = 1.0
+        segmentControl.layer.cornerRadius = 5.0
+        segmentControl.layer.borderColor = UIColor.black.cgColor
+        segmentControl.layer.masksToBounds = true
+        segmentControl.backgroundColor = .white
+        segmentControl.tintColor = .black
+        segmentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.selected)
+        segmentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.black], for: UIControl.State.normal)
+        segmentControl.selectedSegmentTintColor = .black
+        
+        
     }
-
-
+    
+    
 }
 
