c92f95ff1f2a5165d206d1654c66a87dffec76a7
Astrid <tingan.l1216@gmail.com>
2021-08-11 20:52:09 +0800

    turn ifElse to switch statement

--- week3/logInSignUpPage/logInSignUpPage/Status.swift
+++ week3/logInSignUpPage/logInSignUpPage/Status.swift
@@ -9,19 +9,15 @@ import Foundation
 
 enum Status: String {
     
- case accountEmpty = "Account should not be empty."
-  case checkEmpty = "Check Password should not be empty."
- case passwordEmpty = "Password should not be empty."
- case passwordNotEqualToCheck = "SignUp fail"
-   
+    case accountEmpty = "Account should not be empty."
+    case checkEmpty = "Check Password should not be empty."
+    case passwordEmpty = "Password should not be empty."
+    case passwordNotEqualToCheck = "SignUp fail"
     
-  case invalidAccountPassword = "Login fail."
-  
-    
-        
+   case invalidAccountPassword = "Login fail."
 }
 
 struct Valid {
-   static let account = "appworksschool@gmail.com"
+    static let account = "appworksschool@gmail.com"
     static let password = "1234"
 }
--- week3/logInSignUpPage/logInSignUpPage/ViewController.swift
+++ week3/logInSignUpPage/logInSignUpPage/ViewController.swift
@@ -18,38 +18,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         segmentControl.layer.borderWidth = 1.0
         segmentControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.selected)
         segmentControl.selectedSegmentTintColor = .black
         segmentControl.selectedSegmentIndex = 1
     }
-    //    鍵盤之後再處理
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
     @IBAction func switchSegmentControl(_ sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 0 {
+        
+        switch sender.selectedSegmentIndex  {
+        case 0:
             checkLabel.textColor = .gray
             checkTextField.isEnabled = false
             checkTextField.backgroundColor = .darkGray
-        } else {
+            checkTextField.text = ""
+        default:
             checkLabel.textColor = .black
             checkTextField.isEnabled = true
             checkTextField.backgroundColor = .white
-            
         }
     }
     
     
     func popAlert(in status: Status){
         let alert = UIAlertController(title: "Error", message: status.rawValue, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: { [self]_ in
-            accountTextField.text = ""
-            passwordTextField.text = ""
-            checkTextField.text = ""
-        })
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
     }
@@ -57,37 +54,29 @@ class ViewController: UIViewController {
     
     @IBAction func pressButton(_ sender: UIButton) {
         
-//                sign up狀態
+        switch segmentControl.selectedSegmentIndex {
         
-        if  segmentControl.selectedSegmentIndex == 1 {
-            
-            //        sign up account empty
+        case 1:
             guard  accountTextField.text != "" else{
                 popAlert(in: Status.accountEmpty)
                 return
             }
             
-            //        sign up  password empty
-            guard let password =  passwordTextField.text else{ return}
+            guard let password =  passwordTextField.text else{ return }
             if password == ""{
                 popAlert(in: Status.passwordEmpty)
             }
             
-            //        sign up check password empty
-            guard let check =  checkTextField.text else{return }
+            guard let check =  checkTextField.text else{ return }
             if check == "" {
                 popAlert(in: Status.checkEmpty)
-            
-            //            check password
             } else if check != password{
                 popAlert(in: Status.passwordNotEqualToCheck)
             }
-//             log in狀態
-        } else {
             
-            //invalid account,password
+        default:
             guard let account = accountTextField.text,
-                  let passWord = passwordTextField.text else{return}
+                  let passWord = passwordTextField.text else{ return }
             
             if account != Valid.account || passWord != Valid.password {
                 popAlert(in: Status.invalidAccountPassword)
