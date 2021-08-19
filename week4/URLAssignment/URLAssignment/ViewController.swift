a4c016b048c6d6fb8f1f6041edabdd3cf8073d36
Astrid <tingan.l1216@gmail.com>
2021-08-19 21:27:45 +0800

    UI Done with storyboard

---
+++ week4/URLAssignment/URLAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLAssignment
+//
+//  Created by Astrid on 2021/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
