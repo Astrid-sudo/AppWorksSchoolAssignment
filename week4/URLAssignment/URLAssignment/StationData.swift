8f20c29580d8720703aedddad21fb2008725502d
Astrid <tingan.l1216@gmail.com>
2021-08-19 23:08:32 +0800

    complete requirements(havn't MVC)

---
+++ week4/URLAssignment/URLAssignment/StationData.swift
@@ -0,0 +1,15 @@
+//
+//  StationData.swift
+//  URLAssignment
+//
+//  Created by Astrid on 2021/8/19.
+//
+
+import Foundation
+
+struct StationData: Codable {
+    
+    let stationID: String
+    let stationName: String
+    let stationAddress: String
+}
--- week4/URLAssignment/URLAssignment/ViewController.swift
+++ week4/URLAssignment/URLAssignment/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var stationIDLabel: UILabel!
+    @IBOutlet weak var stationNameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchData()
     }
-
-
+    
+    func fetchData(){
+        let url = URL(string: "https://stations-98a59.firebaseio.com/practice.json")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "Get"
+        
+        let session = URLSession(configuration: .default)
+       
+        session.dataTask(with: url) {
+           
+            (data , response, error) in
+           
+            if let httpResponse = response as? HTTPURLResponse,
+               httpResponse.statusCode == 200 {
+                guard let safeData = data else { return }
+                
+                do{
+                    let loadedData =  try JSONDecoder().decode(StationData.self, from: safeData)
+                    print(loadedData)
+                    
+                    DispatchQueue.main.async {
+                        self.stationIDLabel.text = loadedData.stationID
+                        self.stationNameLabel.text = loadedData.stationName
+                        self.addressLabel.text = loadedData.stationAddress
+                    }
+                    
+              } catch {
+                    print("Parse JSON Failed: \(error.localizedDescription)")
+                }
+                
+            } else {
+                print("Error: \((error!.localizedDescription))")
+            }
+        }.resume()
+    }
 }
 
