3ffb066885abd87f92d10a72bfdb134d3dd3a8bf
Astrid <tingan.l1216@gmail.com>
2021-08-22 00:13:02 +0800

    Complete Table Assignment

--- week4/TableView/TableView/TableViewCell.swift
+++ week4/TableView/TableView/TableViewCell.swift
@@ -8,33 +8,33 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-    
-static let reuseIdentifier = String(describing: TableViewCell.self)
-    
- lazy var label: UILabel = {
-        let label = UILabel()
-        return label
-    }()
-   
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-       setupLabel()
-    }
-    
-   required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setupLabel(){
-        let padding : CGFloat = 10
-        contentView.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
-            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
-            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding/2),
-            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding/2),
-            label.heightAnchor.constraint(greaterThanOrEqualToConstant: 101)
-       ])
-}
+  
+  static let reuseIdentifier = String(describing: TableViewCell.self)
+  
+  lazy var label: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setupLabel()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupLabel(){
+    let padding : CGFloat = 10
+    contentView.addSubview(label)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+      label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+      label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding/2),
+      label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -padding/2),
+      label.heightAnchor.constraint(greaterThanOrEqualToConstant: 101)
+    ])
+  }
 }
--- week4/TableView/TableView/ViewController.swift
+++ week4/TableView/TableView/ViewController.swift
@@ -8,51 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    private lazy var tableView: UITableView = {
-        let table = UITableView()
-        table.dataSource = self
-        table.allowsSelection = false
-        table.bounces = false
-        table.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.reuseIdentifier)
-        return table
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setTableView()
-    }
-    
-    private func setTableView(){
-        let safeArea = view.safeAreaLayoutGuide
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
-        ])
-    }
+  
+  private lazy var tableView: UITableView = {
+    let table = UITableView()
+    table.dataSource = self
+    table.allowsSelection = false
+    table.bounces = false
+    table.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.reuseIdentifier)
+    return table
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    setTableView()
+  }
+  
+  private func setTableView(){
+    let safeArea = view.safeAreaLayoutGuide
+    view.addSubview(tableView)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    ])
+  }
 }
 
 //MARK:- UITableView DataSource
 
-extension  ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else {
+      return UITableViewCell()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.reuseIdentifier, for: indexPath) as? TableViewCell else {
-            return UITableViewCell()
-        }
-        
-        cell.label.text = "This is section \(indexPath.section), row \(indexPath.row)"
-        
-        return cell
-    }
+    cell.label.text = "This is section \(indexPath.section), row \(indexPath.row)"
+    
+    return cell
+  }
 }
 
