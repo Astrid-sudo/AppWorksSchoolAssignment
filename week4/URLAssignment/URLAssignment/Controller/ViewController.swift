4379662fa43a38ed7154dfbd5454c117e93cfc88
Astrid <tingan.l1216@gmail.com>
2021-08-21 23:47:21 +0800

    seperate view, complete URLAssignment

--- week4/URLAssignment/URLAssignment/Controller/ViewController.swift
+++ week4/URLAssignment/URLAssignment/Controller/ViewController.swift
@@ -8,82 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
- 
-    var dataManager = DataManager()
-    
-    lazy var stationIDLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 30)
-        return label
-    }()
-   
-    lazy var stationNameLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 80, weight: .thin)
-        return label
-    }()
-   
-    lazy var addressLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 18)
-        label.numberOfLines = 0
-        return label
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor(named: "stationAppColor")
-        setupLabels()
-        dataManager.delegate = self
-        dataManager.performRequest(to:StationDataAddress.r3)
-    }
-    
-    private func setupLabels(){
-        
-        let safeArea = view.safeAreaLayoutGuide
-        view.addSubview(stationIDLabel)
-        view.addSubview(stationNameLabel)
-        view.addSubview(addressLabel)
-        stationIDLabel.translatesAutoresizingMaskIntoConstraints = false
-        stationNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        addressLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            
-            stationIDLabel.topAnchor.constraint(greaterThanOrEqualTo: safeArea.topAnchor),
-            stationIDLabel.bottomAnchor.constraint(equalTo: stationNameLabel.topAnchor,constant: -35),
-            stationIDLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            //大於等於 -100
-            //            stationNameLabel.bottomAnchor.constraint(greaterThanOrEqualTo: view.centerYAnchor, constant: -100),
-            stationNameLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
-            stationNameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            
-            addressLabel.topAnchor.constraint(lessThanOrEqualTo: stationNameLabel.bottomAnchor, constant: 100),
-            //大於等於 -200
-            //            addressLabel.bottomAnchor.constraint(greaterThanOrEqualTo: view.bottomAnchor, constant: -200),
-            addressLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200),
-            addressLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-    }
+  
+  private var dataManager = DataManager()
+  
+  private var stationView = View()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view = stationView
+    dataManager.delegate = self
+    dataManager.performRequest(to: StationDataAddress.r3)
+  
+  }
+  
 }
 //MARK:- DataManagerDelegate
 
 extension ViewController: DataManagerDelegate {
-    
-    func updateUI(with information: StationData) {
-        DispatchQueue.main.async {
-            self.stationIDLabel.text = information.stationID
-            self.stationNameLabel.text = information.stationName
-            self.addressLabel.text = information.stationAddress
-        }
+  
+  func updateUI(with information: StationData) {
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else {return}
+      self.stationView.stationIDLabel.text = information.stationID
+      self.stationView.stationNameLabel.text = information.stationName
+      self.stationView.addressLabel.text = information.stationAddress
     }
-    
-    func failedWithError(error: Error) {
-        print("Parse JSON Failed: \(error.localizedDescription)")
-    }
-    
+  }
+  
+  func failedWithError(error: Error) {
+    print("error: \(error.localizedDescription)")
+  }
+  
 }
--- week4/URLAssignment/URLAssignment/View/View.swift
+++ week4/URLAssignment/URLAssignment/View/View.swift
@@ -9,6 +9,72 @@ import UIKit
 
 class View: UIView {
 
- 
-
+  lazy var stationIDLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .white
+    label.font = .systemFont(ofSize: 30)
+    return label
+  }()
+  
+  lazy var stationNameLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .white
+    label.font = .systemFont(ofSize: 80, weight: .thin)
+    return label
+  }()
+  
+  lazy var addressLabel: UILabel = {
+    let label = UILabel()
+    label.textColor = .white
+    label.font = .systemFont(ofSize: 18)
+    label.numberOfLines = 0
+    return label
+  }()
+  
+  
+  init(){
+    super.init(frame:.zero)
+    backgroundColor = UIColor(named: "stationAppColor")
+    setupStationNameLabel()
+    setupStationIDLabel()
+    setupAddressLabel()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupStationIDLabel() {
+    
+    addSubview(stationIDLabel)
+    stationIDLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      stationIDLabel.bottomAnchor.constraint(equalTo: stationNameLabel.topAnchor, constant: -35),
+      stationIDLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+    ])
+    
+  }
+  
+  private func setupStationNameLabel() {
+    
+    addSubview(stationNameLabel)
+    stationNameLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      stationNameLabel.bottomAnchor.constraint(equalTo: self.centerYAnchor, constant: -100),
+      stationNameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+    ])
+    
+  }
+  
+  private func setupAddressLabel() {
+    
+    addSubview(addressLabel)
+    addressLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      addressLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -200),
+      addressLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+    ])
+    
+  }
+  
 }
